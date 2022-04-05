2afc62aa6276a755d6e68543733d97fdaa975927
laur73 <jj.c.r.1781@gmail.com>
2022-04-05 12:54:47 -0500

    Cambios al controlador de charts

---
+++ AdminLTE/Controllers/ChartsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminLTE.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public IActionResult Chartjs()
+        {
+            return View();
+        }
+
+        public IActionResult Flot()
+        {
+            return View();
+        }
+    }
+}
