7a36621ac143837c52544d5a8908febbeae9f50c
laur73 <jj.c.r.1781@gmail.com>
2022-05-12 20:18:19 -0500

    Metodos async await

--- AdminLTE/Repositorios/RepositorioApoyos.cs
+++ AdminLTE/Repositorios/RepositorioApoyos.cs
@@ -1,154 +1,98 @@
 ﻿using AdminLTE.Models;
-using System.Data;
-using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace AdminLTE.Repositorios
 {
-    public class RepositorioApoyos
+    //Interfaz que va a contener los metodos
+    public interface IRepositorioApoyos
     {
-        public List<ApoyoViewModel> Listar()
+        Task Actualizar(ApoyoViewModel apoyo);
+        Task Eliminar(int IdApoyo);
+        Task Crear(ApoyoViewModel apoyo);
+        Task<bool> Existe(string nombre);
+        Task<IEnumerable<ApoyoViewModel>> Listar();
+        Task<ApoyoViewModel> ObtenerId(int id);
+    }
+
+    //Heredamos la interfaz
+    public class RepositorioApoyos : IRepositorioApoyos
+    {
+        //Declaramos la variable de cadena de conexion de solo lectura
+        private readonly string connectionString;
+
+        //Constructor para poder acceder al las propiedades del IConfiguration
+        public RepositorioApoyos(IConfiguration configuration)
         {
-            var lista = new List<ApoyoViewModel>();
-            var con = new Conexion();
-
-            using (var conexion = new SqlConnection(con.getCadenaSQL()))
-            {
-                conexion.Open();
-                SqlCommand cmd = new SqlCommand("sp_ListarApoyos", conexion);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                using (var dr = cmd.ExecuteReader())
-                {
-                    while (dr.Read())
-                    {
-                        lista.Add(new ApoyoViewModel()
-                        {
-                            IdApoyo = Convert.ToInt32(dr["IdApoyo"]),
-                            Nombre = dr["Nombre"].ToString(),
-                            Descripcion = dr["Descripcion"].ToString()
-                        });
-                    }
-                }
-            }
-            return lista;
+            //Cadena de conexion
+            connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        public ApoyoViewModel ObtenerId(int IdApoyo)
+        //Async await para programacion asincrona: metodos asincronos
+
+        //Método para Listar los Apoyos
+        public async Task<IEnumerable<ApoyoViewModel>> Listar()
         {
-            var apoyo = new ApoyoViewModel();
-            var con = new Conexion();
-
-            using (var conexion = new SqlConnection(con.getCadenaSQL()))
-            {
-                conexion.Open();
-                SqlCommand cmd = new SqlCommand("sp_ObtenerIdApoyo", conexion);
-                cmd.Parameters.AddWithValue("IdApoyo", IdApoyo);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                using (var dr = cmd.ExecuteReader())
-                {
-                    while (dr.Read())
-                    {
-                        apoyo.IdApoyo = Convert.ToInt32(dr["IdApoyo"]);
-                        apoyo.Nombre = dr["Nombre"].ToString();
-                        apoyo.Descripcion = dr["Descripcion"].ToString();
-                    }
-                }
-            }
-
-            return apoyo;
+            //Abrimos la cadena de conexión
+            using var connection = new SqlConnection(connectionString);
+            //Usamos QueryAsync pporque permite realizar un Select
+            return await connection.QueryAsync<ApoyoViewModel>(@"Apoyos_Listar", commandType: System.Data.CommandType.StoredProcedure);
         }
 
-        public bool Crear(ApoyoViewModel apoyo)
+        //Método para saber si ya existe un Apoyo
+        public async Task<bool> Existe(string nombre)
         {
-            bool rpta;
-
-            try
-            {
-                var con = new Conexion();
-
-                using (var conexion = new SqlConnection(con.getCadenaSQL()))
-                {
-                    conexion.Open();
-                    SqlCommand cmd = new SqlCommand("sp_CrearApoyo", conexion);
-                    cmd.Parameters.AddWithValue("Nombre", apoyo.Nombre);
-                    cmd.Parameters.AddWithValue("Descripcion", apoyo.Descripcion);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    //Ejecutamos el procedimiento almacenado
-                    cmd.ExecuteNonQuery();
-                }
-
-                rpta = true;
-            }
-            catch (Exception e)
-            {
-                string error = e.Message;
-                rpta = false;
-            }
-
-            return rpta;
+            using var connection = new SqlConnection(connectionString);
+            //El FirstOrDefaul quiere decir que va a traer lo que encuentre primero o un valor por defecto por eso traemos un int
+            var existe = await connection.QueryFirstOrDefaultAsync<int>($@"SELECT 1 FROM Apoyos WHERE Nombre = @Nombre", new { nombre });
+            return existe == 1;
         }
 
-        public bool Editar(ApoyoViewModel apoyo)
+        //Método para Insertar un Apoyo
+        public async Task Crear(ApoyoViewModel apoyo)
         {
-            bool rpta;
+            //Abrimos la cadena de conexión
+            using var connection = new SqlConnection(connectionString);
 
-            try
-            {
-                var con = new Conexion();
-
-                using (var conexion = new SqlConnection(con.getCadenaSQL()))
+            //QuerySingleAsync quiere decir que nos va a traer un solo resultado
+            //El Scope Identity trae el Id del registro recién creado (esta dentro del sp)
+            var id = await connection.QuerySingleAsync<int>(@"Apoyos_Crear",
+                new
                 {
-                    conexion.Open();
-                    SqlCommand cmd = new SqlCommand("sp_EditarApoyo", conexion);
-                    cmd.Parameters.AddWithValue("IdApoyo", apoyo.IdApoyo);
-                    cmd.Parameters.AddWithValue("Nombre", apoyo.Nombre);
-                    cmd.Parameters.AddWithValue("Descripcion", apoyo.Descripcion);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.ExecuteNonQuery();
-                }
-
-                rpta = true;
-            }
-            catch (Exception e)
-            {
-                string error = e.Message;
-                rpta = false;
-            }
-
-            return rpta;
+                    apoyo.Nombre,
+                    apoyo.Descripcion,
+                    apoyo.Cantidad
+                },
+                commandType: System.Data.CommandType.StoredProcedure);
+            apoyo.IdApoyo = id;
         }
 
-        public bool Eliminar(int IdApoyo)
+        //Método para Obtener el Id de los Apoyos para poder Actualizar o Eliminar
+        public async Task<ApoyoViewModel> ObtenerId(int IdApoyo)
         {
-            bool rpta;
-
-            try
-            {
-                var con = new Conexion();
-
-                using (var conexion = new SqlConnection(con.getCadenaSQL()))
-                {
-                    conexion.Open();
-                    SqlCommand cmd = new SqlCommand("sp_EliminarApoyo", conexion);
-                    cmd.Parameters.AddWithValue("IdApoyo", IdApoyo);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.ExecuteNonQuery();
-                }
-
-                rpta = true;
-            }
-            catch (Exception e)
-            {
-                string error = e.Message;
-                rpta = false;
-            }
-
-            return rpta;
+            //Abrimos la cadena de conexión
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<ApoyoViewModel>(
+                @"Apoyos_ObtenerId", new { IdApoyo }, commandType: System.Data.CommandType.StoredProcedure);
         }
+
+        //Método para Actualizar un Apoyo
+        public async Task Actualizar(ApoyoViewModel apoyo)
+        {
+            //Abrimos la cadena de conexión
+            using var connection = new SqlConnection(connectionString);
+            //ExecuteAsync quiere decir que solo vamos a realizar una consulta y no retorna nada
+            await connection.ExecuteAsync(@"Apoyos_Actualizar", apoyo, commandType: System.Data.CommandType.StoredProcedure);
+        }
+
+        //Método para Eliminar un Apoyo
+        public async Task Eliminar(int IdApoyo)
+        {
+            //Abrimos la cadena de conexión
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"Apoyos_Eliminar", new { IdApoyo }, commandType: System.Data.CommandType.StoredProcedure);
+        }
+
 
     }
 }
